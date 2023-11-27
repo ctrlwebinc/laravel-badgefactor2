aca281ec0b91c157a87aa0e33c15fe76d182ea1e
Daniel Le Blanc <daniel.leblanc@ctrlweb.ca>
2023-11-27 13:46:55 -0500

    No image field in BadgeCategory.

--- src/Models/Badges/BadgeCategory.php
+++ src/Models/Badges/BadgeCategory.php
@@ -3,15 +3,13 @@
 namespace Ctrlweb\BadgeFactor2\Models\Badges;
 
 use Ctrlweb\BadgeFactor2\Models\BadgeCategory as BadgeFactor2BadgeCategory;
-use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Spatie\Translatable\HasTranslations;
 
-class BadgeCategory extends BadgeFactor2BadgeCategory implements HasMedia
+class BadgeCategory extends BadgeFactor2BadgeCategory
 {
     use HasTranslations;
-    use InteractsWithMedia;
 
     public static function findBySlug($slug)
     {
@@ -25,11 +23,4 @@ class BadgeCategory extends BadgeFactor2BadgeCategory implements HasMedia
         'slug',
         'description',
     ];
-
-    public function registerMediaConversions(Media $media = null): void
-    {
-        $this->addMediaConversion('thumb')
-            ->width(32)
-            ->height(32);
-    }
 }
