8c7779ea304f20a8a9dfe98566a1f83b37b999eb
Daniel Le Blanc <daniel.leblanc@ctrlweb.ca>
2023-12-07 07:24:34 -0500

    Auto-generating scout index.

--- src/Http/Controllers/Api/SearchController.php
+++ src/Http/Controllers/Api/SearchController.php
@@ -7,15 +7,34 @@ use Ctrlweb\BadgeFactor2\Models\Courses\Course;
 use Ctrlweb\BadgeFactor2\Models\Courses\CourseGroup;
 use Ctrlweb\BadgeFactor2\Models\Courses\CourseGroupCategory;
 use Illuminate\Http\Request;
+use Meilisearch\Exceptions\ApiException;
 
 class SearchController extends Controller
 {
     public function __invoke(string $locale, Request $request, string $string)
     {
         $search = [];
-        $search['courses'] = Course::search($string)->get();
-        $search['course_groups'] = CourseGroup::search($string)->get();
-        $search['course_group_categories'] = CourseGroupCategory::search($string)->get();
+
+        try {
+            $search['courses'] = Course::search($string)->get();
+        } catch (ApiException $e) {
+            Course::all()->searchable();
+            $search['courses'] = Course::search($string)->get();
+        }
+
+        try {
+            $search['course_groups'] = CourseGroup::search($string)->get();
+        } catch (ApiException $e) {
+            CourseGroup::all()->searchable();
+            $search['course_groups'] = CourseGroup::search($string)->get();
+        }
+
+        try {
+            $search['course_group_categories'] = CourseGroupCategory::search($string)->get();
+        } catch (ApiException $e) {
+            CourseGroupCategory::all()->searchable();
+            $search['course_group_categories'] = CourseGroupCategory::search($string)->get();
+        }
 
         return $search;
     }
