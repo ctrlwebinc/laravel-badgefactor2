39a862bef3cdba25317a404f78ef50eddad31261
Daniel Le Blanc <daniel.leblanc@ctrlweb.ca>
2023-12-13 07:05:02 -0500

    Migration bug fix.

--- database/migrations/2023_11_30_092151_update_badge_pages_table.php
+++ database/migrations/2023_11_30_092151_update_badge_pages_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use Ctrlweb\BadgeFactor2\Models\Badges\BadgePage;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
