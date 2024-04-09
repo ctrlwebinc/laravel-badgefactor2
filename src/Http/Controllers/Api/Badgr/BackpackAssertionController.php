db52339293e056e5658492c9d71212e30bb5b1b9
Daniel Le Blanc <daniel.leblanc@ctrlweb.ca>
2024-04-09 15:37:13 -0400

    Missing use.

--- src/Http/Controllers/Api/Badgr/BackpackAssertionController.php
+++ src/Http/Controllers/Api/Badgr/BackpackAssertionController.php
@@ -2,6 +2,7 @@
 
 namespace Ctrlweb\BadgeFactor2\Http\Controllers\Api\Badgr;
 
+use App\Http\Requests\AssertionVisibilityRequest;
 use Ctrlweb\BadgeFactor2\Http\Controllers\Controller;
 use Ctrlweb\BadgeFactor2\Models\Badgr\Assertion;
 use Ctrlweb\BadgeFactor2\Models\Badgr\AssertionUser;
