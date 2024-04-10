98b06833442611d9b5d749c9c9b5d6730369e3e6
Daniel Le Blanc <daniel.leblanc@ctrlweb.ca>
2024-04-10 11:42:26 -0400

    Allowing anonymous badge recipients.

--- src/Http/Resources/Badgr/AssertionResource.php
+++ src/Http/Resources/Badgr/AssertionResource.php
@@ -3,6 +3,7 @@
 namespace Ctrlweb\BadgeFactor2\Http\Resources\Badgr;
 
 use App\Http\Resources\LearnerPublicResource;
+use Ctrlweb\BadgeFactor2\Models\Badgr\AssertionUser;
 use Ctrlweb\BadgeFactor2\Models\User;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -17,13 +18,42 @@ class AssertionResource extends JsonResource
      */
     public function toArray($request)
     {
+        $recipient = LearnerPublicResource::make(User::where('email', '=', $this['recipient']['plaintextIdentity'])->first());
+        $visibility = AssertionUser::where('assertion_id', '=', $this['entityId'])->where('user_id', '=', $recipient->id)->first();
+        if ($visibility->is_visible) {
+            return [
+                'id'         => $this['entityId'],
+                'issued_on'  => $this['issuedOn'],
+                'badgeclass' => $this['badgeclass'],
+                'issuer'     => $this['issuer'],
+                'image'      => $this['image'],
+                'recipient'  => LearnerPublicResource::make(User::where('email', '=', $this['recipient']['plaintextIdentity'])->first()),
+            ];
+        }
         return [
             'id'         => $this['entityId'],
             'issued_on'  => $this['issuedOn'],
             'badgeclass' => $this['badgeclass'],
             'issuer'     => $this['issuer'],
             'image'      => $this['image'],
-            'recipient'  => LearnerPublicResource::make(User::where('email', '=', $this['recipient']['plaintextIdentity'])->first()),
+            'recipient'  => [
+                'slug' => null,
+                'username' => null,
+                'first_name' => 'Utilisateur',
+                'last_name' => 'Anonyme',
+                'description' => null,
+                'website' => null,
+                'establishment' => null,
+                'place' => null,
+                'organisation' => null,
+                'job' => null,
+                'biography' => null,
+                'facebook' => null,
+                'twitter' => null,
+                'linkedin' => null,
+                'photo' => null,
+                'last_connexion' => null,
+            ]
         ];
     }
 }
