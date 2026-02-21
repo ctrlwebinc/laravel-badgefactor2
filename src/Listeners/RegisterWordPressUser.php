aac836b3dca4726287f2553c7496a64e18f02a02
editho512 <alex.editho512@gmail.com>
2026-02-21 17:12:38 +0300

    Wordpress user registration - update

--- src/Listeners/RegisterWordPressUser.php
+++ src/Listeners/RegisterWordPressUser.php
@@ -6,6 +6,7 @@ use Ctrlweb\BadgeFactor2\Events\UserRegistered;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class RegisterWordPressUser implements ShouldQueue
 {
